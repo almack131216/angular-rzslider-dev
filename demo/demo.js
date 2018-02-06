4de8f1ac2283a1255b3973a28842640da4d82d1a
ecp-alex <alex.mackenzie@englishcollege.cz>
2018-02-06 15:09:50 +0100

    r11 | add padding to wrapper + position btns

--- demo/demo.js
+++ demo/demo.js
@@ -34,19 +34,11 @@ app.controller('MainCtrl', function ($scope, $rootScope, $timeout, $uibModal) {
   /* + button triggered outside of directive element */
   $scope.parentBtnPlus = function () {
     $scope.amcustDebugSlider.options.optionBtnPlus();
-    // if ($scope.amcustDebugSlider.value < $scope.amcustDebugSlider.options.ceil) {
-    //   console.log('!!! amcust: parentBtnPlus() [' + $scope.amcustDebugSlider.value + ' -> ' + ($scope.amcustDebugSlider.value + 1) + ']');
-    //   $scope.amcustDebugSlider.value++;
-    // }
   }
 
   /* - button triggered outside of directive element */
   $scope.parentBtnMinus = function () {
     $scope.amcustDebugSlider.options.optionBtnMinus();
-    // console.log('!!! amcust: parentBtnMinus() [' + $scope.amcustDebugSlider.value + ' -> ' + ($scope.amcustDebugSlider.value - 1) + ']');
-    // if ($scope.amcustDebugSlider.value > $scope.amcustDebugSlider.options.floor) {
-    //   $scope.amcustDebugSlider.value--;
-    // }
   }
 
   /* 
@@ -57,20 +49,4 @@ app.controller('MainCtrl', function ($scope, $rootScope, $timeout, $uibModal) {
 }
 */
 
-})
-
-app.directive('clickableLabel', function () {
-  return {
-    restrict: 'E',
-    scope: { label: '=' },
-    replace: true,
-    template:
-    "<button ng-click='onclick(label)' style='cursor: pointer;'>click me - {{label}}</button>",
-    link: function (scope, elem, attrs) {
-      scope.onclick = function (label) {
-        console.log("I'm " + label);
-        scope.amcustDebugSlider.options.showAdvice = 1;
-      }
-    },
-  }
 })
